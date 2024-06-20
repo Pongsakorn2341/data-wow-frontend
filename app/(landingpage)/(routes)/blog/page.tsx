274c2622c94823913a8c0d23853bb36a88cc9bb2
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-20 13:24:43 +0700

    fix: build failed

--- app/(landingpage)/(routes)/blog/page.tsx
+++ app/(landingpage)/(routes)/blog/page.tsx
@@ -1,11 +1,17 @@
+"use server";
+
 import BlogList from "@/components/blog/BlogList";
 import BlogSearch from "@/components/blog/BlogSearch";
+import LoadingComponent from "@/components/common/LoadingComponent";
+import { Suspense } from "react";
 
 const blogDetailPage = async () => {
   return (
     <section className="relative sm:p-4 block  max-w-[800px] sm:max-w-[1000px]">
-      <BlogSearch />
-      <BlogList isOwn={true} />
+      <Suspense fallback={<LoadingComponent />}>
+        <BlogSearch />
+        <BlogList isOwn={true} />
+      </Suspense>
     </section>
   );
 };
--- app/(landingpage)/(routes)/page.tsx
+++ app/(landingpage)/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import BlogList from "@/components/blog/BlogList";
 import BlogSearch from "@/components/blog/BlogSearch";
+import LoadingComponent from "@/components/common/LoadingComponent";
+import { Suspense } from "react";
 
 const HomePage = async () => {
   return (
     <section className="relative sm:p-4 block  max-w-[800px] sm:max-w-[1000px]">
-      <BlogSearch />
-      <BlogList isOwn={false} />
+      <Suspense fallback={<LoadingComponent />}>
+        <BlogSearch />
+        <BlogList isOwn={false} />
+      </Suspense>
     </section>
   );
 };
