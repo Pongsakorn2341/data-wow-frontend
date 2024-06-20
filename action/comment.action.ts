81c72b7f780ac1265a06045af2348d98ee1d2591
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-20 11:07:05 +0700

    chore: update comment action

---
+++ action/comment.action.ts
@@ -0,0 +1,78 @@
+import { getAccessToken } from "./common.action";
+
+type IAddComment = {
+  blog_id: string;
+  detail: string;
+};
+
+export const createComment = async (payload: IAddComment) => {
+  const accessToken = await getAccessToken();
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", `Bearer ${accessToken}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/comment`,
+    {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify(payload),
+    }
+  );
+  const result = await response.json();
+  return result;
+};
+
+type IUpdateComment = {
+  detail: string;
+};
+
+// export const updateComment = async (
+//   commentId: string,
+//   payload: IUpdateComment
+// ) => {
+//   const accessToken = await getAccessToken();
+//   const headers = new Headers();
+//   headers.append("Content-Type", "application/json");
+//   headers.append("Authorization", `Bearer ${accessToken}`);
+//   const response = await fetch(
+//     `${process.env.NEXT_PUBLIC_BACKEND_URL}/blog/${commentId}`,
+//     {
+//       headers: headers,
+//       method: "PATCH",
+//       body: JSON.stringify(payload),
+//     }
+//   );
+//   const result = await response.json();
+//   return result;
+// };
+
+// export const deleteComment = async (blogId: string) => {
+//   const accessToken = await getAccessToken();
+//   const headers = new Headers();
+//   headers.append("Content-Type", "application/json");
+//   headers.append("Authorization", `Bearer ${accessToken}`);
+//   const response = await fetch(
+//     `${process.env.NEXT_PUBLIC_BACKEND_URL}/blog/${blogId}`,
+//     {
+//       headers: headers,
+//       method: "DELETE",
+//     }
+//   );
+//   const result = await response.json();
+//   return result;
+// };
+
+export const getComments = async () => {
+  const accessToken = await getAccessToken();
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", `Bearer ${accessToken}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/comment`,
+    {
+      headers: headers,
+    }
+  );
+  const result = await response.json();
+  return result;
+};
