6cc1a68e36659701230a140a7abd273c798099ce
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-20 11:15:48 +0700

    chore: category data

---
+++ lib/data/category.data.ts
@@ -0,0 +1,31 @@
+export const CategoryList = [
+  {
+    label: "History",
+    value: "history",
+  },
+  {
+    label: "Food",
+    value: "food",
+  },
+  {
+    label: "Pets",
+    value: "pets",
+  },
+  {
+    label: "Health",
+    value: "health",
+  },
+
+  {
+    label: "Fashion",
+    value: "fashion",
+  },
+  {
+    label: "Exercise",
+    value: "exercise",
+  },
+  {
+    label: "Others",
+    value: "others",
+  },
+];
