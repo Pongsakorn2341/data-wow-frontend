30b9f40634600010bee575e373c1429f70a9c10a
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-20 11:16:02 +0700

    style (ui): add homepages

--- app/(landingpage)/(routes)/page.tsx
+++ app/(landingpage)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import BlogList from "@/components/blog/BlogList";
+import BlogSearch from "@/components/blog/BlogSearch";
+
 const HomePage = () => {
   return (
     <section className="relative sm:p-4 block  max-w-[800px] sm:max-w-[1000px]">
-      TEST MAIN PAGE
+      <BlogSearch />
+      <BlogList />
     </section>
   );
 };
