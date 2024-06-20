9cc4356d691c51bc0e513ea5bcc75fa11e6c0829
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-20 10:48:42 +0700

    chore: add pages

---
+++ app/(landingpage)/(routes)/[blog]/[blogId]/page.tsx
@@ -0,0 +1,11 @@
+const blogDetailPage = () => {
+  return (
+    <section className="bg-white min-h-[calc(100dvh-72px)] p-4 block md:top-16">
+      <div className="container max-w-[800px] sm:max-w-[1000px]">
+        Blog Detail
+      </div>
+    </section>
+  );
+};
+
+export default blogDetailPage;
---
+++ app/(landingpage)/(routes)/[blog]/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <section className="mt-6 p-4">Our blog</section>;
+};
+
+export default page;
